b7a84e3a67ce379e91ddceaa25d9656ae3bc5e88
abv4594 <abv4594@gmail.com>
2021-06-03 13:16:00 +0300

    Done render method for class Board

--- lib/board.rb
+++ lib/board.rb
@@ -13,6 +13,15 @@ class Board
         @grid[row][pos]
     end
 
+    def render
+        puts (0..9).to_a.join(' ')
+        @grid.each_with_index do |row,i|
+            print i.to_s + ' '
+            row.each {|square| print square.to_s + ' '}
+            print "\n"
+        end
+    end
+
 
 end
 
