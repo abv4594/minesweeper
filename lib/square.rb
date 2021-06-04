54eb9d2d8fef2e45880148930b628a4f80a24259
abv4594 <abv4594@gmail.com>
2021-06-04 15:31:28 +0300

    Done get neighbors logic in Square class

--- lib/square.rb
+++ lib/square.rb
@@ -1,14 +1,31 @@
 class Square
+
+    DELTAS = [[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1]]
+
     def initialize(board, pos)
         @bomb = false 
         @flagged = false
         @revealed = false
         @pertaining_board = board
-        @my_pos = pos
+        @neighbors = Square.get_neighbors(pos)
+    end
+    def self.get_neighbors(pos)
+        neighbors = []
+        row, col = pos
+        DELTAS.each do |delta|
+            delta_row, delta_col = delta
+            new_row = row + delta_row
+            new_col = col + delta_col
+            if (0..8).include?(new_row) && (0..8).include?(new_col)
+                neighbors << [new_row, new_col]
+            end
+        end
+        neighbors
     end
     def inspect
-        {'has a bomb?' => @bomb, 'flagged?' => @flagged, 'revealed?' => @revealed, 'my_pos' => @my_pos}
+        {'has a bomb?' => @bomb, 'flagged?' => @flagged, 'revealed?' => @revealed, 'neighbors' => @neighbors}
     end
+
     def to_s
         if @revealed
             return 'B' if @bomb 
